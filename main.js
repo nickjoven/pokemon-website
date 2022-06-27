12a1956ef0ff7f1055e77fe40f4e240431d262c5
Nicholas <nicholasjoven@gmail.com>
2022-06-27 17:05:24 -0400

    added js

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-alert('JavaScript is working')
+const pokemon = ['charmander', 'bulbasaur', 'squirtle',]
+
+const pokemonIDs = ['001', '002', '003', '257', '700',]
+
+// DOM = Document Object Model
+// loop over every ID
+// create an HTML element
+// set the HTML element values
+// append HTML element to the DOM
+
+pokemonIDs.map((id) => {
+    let imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${id}.png`
+    console.log('img url', imgUrl)
+    let img = document.createElement('img')
+    img.src = imgUrl
+    img.setAttribute('height', '100px')
+    img.setAttribute('width', '100px')
+    document.body.append(img)
+})
