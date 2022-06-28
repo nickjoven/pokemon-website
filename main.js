2fc8d029b917df5e6396420b3c6a48f17a1b15f0
Nicholas <nicholasjoven@gmail.com>
2022-06-28 13:29:29 -0400

    created roster container

--- main.js
+++ main.js
@@ -4,6 +4,13 @@ const pokemon = [
     {name: 'Squirtle', id: '007'},
 ]
 const containerDiv = document.querySelector('#container')
+const newBtn = document.querySelector('#new-pokemon-btn')
+const rosterDiv = document.querySelector('#roster')
+
+newBtn.addEventListener('click', () => {
+    let num = prompt('ENTER A POKEMON NUMBER')
+    console.log('Number entered', num)
+})
 
 // DOM = Document Object Model
 // loop over every ID
@@ -19,10 +26,25 @@ pokemon.map((element, index) => {
     h3.innerText = element.name
     div.setAttribute('class', 'pokemon-card') //setting the class of the div
     let img = document.createElement('img') // creating the img element itself
+    let audioUrl = `https://play.pokemonshowdown.com/audio/cries/${element.name.toLowerCase()}.mp3`
+    let audio = document.createElement('audio')
+    let source = document.createElement('source')
+    source.setAttribute('src', audioUrl)
+    source.setAttribute('type', 'audio/mpeg')
+    audio.append(source)
+    div.addEventListener('click', () => {
+        console.log('audio', audioUrl)
+        audio.play();
+    })
     img.src = imgUrl
-    div.append(img, h3)
+    div.append(img, h3), audio
     containerDiv.append(div)
 })
 
-// 
+// below let div =...
+// let h3 = document.CreateElement.('h3')
+// h3.innterText = element.name
 
+// add h3 to your div.append => div.append(img, h3)
+
+// add, commit and push changes to github
